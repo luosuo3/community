e35fef30c159a6edf318698f103d4c1779e7afd7
luosuo3 <wzlx718652232@outlook.com>
2019-07-21 14:08:40 +0800

    init repo

---
+++ src/main/java/club/luosuo/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package club.luosuo.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wzlx7
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/club/luosuo/community/controller/IndexController.java
@@ -0,0 +1,19 @@
+package club.luosuo.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author wzlx7
+ */
+@Controller
+public class IndexController
+{
+    @GetMapping("/")
+    public String index() {
+
+        return "index";
+    }
+}
